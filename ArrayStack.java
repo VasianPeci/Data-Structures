8ef05bda1281401818ba3c3e4ff49cf84803be34
VasianPeci <140719484+VasianPeci@users.noreply.github.com>
2025-07-01 21:50:31 +0200

    Update ArrayStack.java with useful methods

--- ArrayStack.java
+++ ArrayStack.java
@@ -54,4 +54,75 @@ public class ArrayStack<AnyType extends Comparable<AnyType>> {
         array = newArray;
     }
 
+    // size of stack
+    public int size(){
+        return topOfStack + 1;
+    }
+
+    // print stack
+    public void printStack(){
+        if (topOfStack == -1) {
+            System.out.println("Stack is empty");
+        } else {
+            for (int i = topOfStack; i >= 0; i--) {
+                System.out.print(array[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    // sort stack
+    public void sortStack(){
+        for (int i = 0; i <= topOfStack; i++) {
+            for (int j = 0; j < topOfStack-i; j++) {
+                if (array[j+1].compareTo(array[j]) < 0) {
+                    AnyType temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+
+    // copy to other stack
+    public void copyToOtherStack(ArrayStack<AnyType> otherStack){
+        otherStack.makeEmpty();
+        for (int i = 0; i <= topOfStack; i++) {
+            otherStack.push(array[i]);
+        }
+    }
+
+    // max binary number (we suppose stacks contain binary numbers where each element is either 1 or 0)
+    public static <AnyType extends Comparable<AnyType>> int maxBinaryNumber(ArrayStack<Integer> stack1, ArrayStack<Integer> stack2){
+        // find stack1 number in decimal
+        int number1 = 0;
+        for (int i = stack1.topOfStack; i >= 0; i--) {
+            number1 += Math.pow(2, i) * stack1.array[i];
+        }
+
+        // find stack2 number in decimal
+        int number2 = 0;
+        for (int i = stack2.topOfStack; i >= 0; i--) {
+            number2 += Math.pow(2, i) * stack2.array[i];
+        }
+
+        // compare both numbers
+        if (number1 > number2) {
+            return number1;
+        }
+        return number2;
+    }
+
+    // move element at k index to top of stack
+    public void moveElementToTop(int k){
+        if (k < 0 || k > topOfStack) {
+            System.out.println("Index k out of bounds");
+            return;
+        }
+        for (int i = k; i < topOfStack; i++) {
+            AnyType temp = array[i];
+            array[i] = array[i+1];
+            array[i+1] = temp;
+        }
+    }
 }
