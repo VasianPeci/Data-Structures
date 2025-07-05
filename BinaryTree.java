01ac12a2d71cd12d77a937277e4db49cc46ea7d2
VasianPeci <140719484+VasianPeci@users.noreply.github.com>
2025-07-05 23:58:44 +0200

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -63,4 +63,105 @@ public class BinaryTree<AnyType> {
             tree2.makeEmpty();
         }
     }
+
+    // max value in binary tree
+    public int maxValue(BinaryNode<Integer> root) {
+        if (root == null) {
+            return Integer.MIN_VALUE;
+        }
+        int max = root.getElement();
+        int leftMax = maxValue(root.getLeft());
+        int rightMax = maxValue(root.getRight());
+
+        return Math.max(max, Math.max(leftMax, rightMax));
+    }
+
+    // print even numbers using preOrder traversal
+    public void printEven(BinaryNode<Integer> node) {
+        if (node == null) {
+            System.out.println("Tree is empty");
+            return;
+        }
+        if (node.getElement() % 2 == 0) {
+            System.out.print(node.getElement() + " ");
+        }
+        if (node.getLeft() != null) {
+            printEven(node.getLeft());
+        }
+        if (node.getRight() != null) {
+            printEven(node.getRight());
+        }
+    }
+
+    // find parent of element x
+    public BinaryNode<AnyType> parentOf(AnyType x, BinaryNode<AnyType> node) {
+        if (node.getElement().equals(x)) throw new RuntimeException("Root does not have parent");
+        if (node.getLeft() == null && node.getRight() == null) {
+            return null;
+        }
+        if (node.getLeft().getElement().equals(x) || node.getRight().getElement().equals(x)) {
+            return node;
+        }
+        return parentOf(x, node.getLeft()) != null ? parentOf(x, node.getLeft()) : (parentOf(x, node.getRight()) != null ? parentOf(x, node.getRight()) : null);
+    }
+
+    // number of leaf nodes in a tree
+    public int leafNodes(BinaryNode<AnyType> root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.getLeft() == null && root.getRight() == null) {
+            return 1;
+        }
+        return leafNodes(root.getLeft()) + leafNodes(root.getRight());
+    }
+
+    // number of full nodes
+    public int fullNodes(BinaryNode<AnyType> root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.getLeft() != null && root.getRight() != null) {
+            return 1 + fullNodes(root.getLeft()) + fullNodes(root.getRight());
+        }
+        return fullNodes(root.getRight()) + fullNodes(root.getLeft());
+    }
+
+    // method for removing nodes in a tree and returning that tree
+    public BinaryNode<AnyType> removeLeafNodes(BinaryNode<AnyType> root) {
+        if (root == null || (root.getLeft() == null && root.getRight() == null)) {
+            return null;
+        }
+        root.setLeft(removeLeafNodes(root.getLeft()));
+        root.setRight(removeLeafNodes(root.getRight()));
+        return root;
+    }
+
+    // find number of pairs parent-child whose sum is a number k
+    public int sumPairs(int k, BinaryNode<Integer> root) {
+        if (root == null) return 0;
+        if ((root.getLeft() != null && root.getElement() + root.getLeft().getElement() == k) || (root.getRight() != null && root.getElement() + root.getRight().getElement() == k)) {
+            return 1 + sumPairs(k, root.getLeft()) + sumPairs(k, root.getRight());
+        }
+        return sumPairs(k, root.getLeft()) + sumPairs(k, root.getRight());
+    }
+
+    // depth of a node with value k
+    public int depthOf(AnyType k, BinaryNode<AnyType> root) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.getElement().equals(k)) {
+            return 0;
+        }
+        int leftDepth = depthOf(k, root.getLeft());
+        if (leftDepth != -1) {
+            return leftDepth + 1;
+        }
+        int rightDepth = depthOf(k, root.getRight());
+        if (rightDepth != -1) {
+            return rightDepth + 1;
+        }
+        return -1;
+    }
 }
